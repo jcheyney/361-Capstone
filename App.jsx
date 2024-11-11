d8ded8ca16e59facb84a3488d05f6c6561869ca4
jcheyney <111913781+jcheyney@users.noreply.github.com>
2024-11-11 12:46:20 -0600

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import Product from './Product.jsx'
+function App() {
+  return(
+    <>
+    <Header></Header>
+    <Product></Product>
+    <Product></Product>
+    <Product></Product>
+    <Product></Product>
+    <Product></Product>
+    <Product></Product>
+    <Product></Product>
+    <Footer></Footer>
+    </>
+  );
+}
+
+export default App
---
+++ Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+return(
+    <footer>
+        <p>&copy; {new Date().getFullYear()} College Clutter</p>
+    </footer>
+);
+}
+export default Footer
