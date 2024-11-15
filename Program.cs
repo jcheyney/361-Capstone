0a36fb40d6b049753bc9d3b3cf89944ec38204aa
omuenster2 <123435493+omuenster2@users.noreply.github.com>
2024-11-15 12:13:03 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+namespace Tutorial.SqlConn
+{
+//class DBSQLServerUtils
+//{
+//public static SqlConnection
+////GetDBConnection(string datasource, string database, string
+//username, string password)
+//{
+//string connString = @"Data Source=" + datasource + ";Initial Catalog="
+//+ database + ";Persist Security Info=True;User ID=" +
+//username + ";Password=" + password + "Encrypt=False;";
+//SqlConnection conn = new SqlConnection(connString);
+//return conn;
+//}
+//}
+public class DBUtils
+{
+public static SqlConnection GetDBConnection()
+{
+string datasource = @"localhost";
+string database = "msdb";
+string username = "sa";
+string password = "dockerStrongPwd123";
+string connString = @"Data Source=" + datasource + ";Initial Catalog="
++ database + ";Persist Security Info=True;User ID=" +
+username + ";Password=" + password + "Encrypt=False;";
+var ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+SqlConnection conn = new SqlConnection(ConnectionString);
+return conn;
+}
+}
+class Program
+{
+static void Main(string[] args)
+{
+Console.WriteLine("Getting Connection ...");
+using (SqlConnection conn = DBUtils.GetDBConnection())
+{
+try
+{
+Console.WriteLine("Opening Connection ...");
+conn.Open();
+Console.WriteLine("Connection successful!");
+}
+catch (Exception e)
+{
+Console.WriteLine("Error: " + e.Message);
+}
+}
+Console.ReadLine(); // Wait for user input before closing console
+}
+}
+}
