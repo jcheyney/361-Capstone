2d0b34e4b5eb8891259eda8628a4367ebea684d4
Hze Zh <149043451+Zh-hz2@users.noreply.github.com>
2024-12-07 14:00:50 -0600

    Header navigation bar icons for Cart and Login

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,35 +10,38 @@ const Header = ({ setPage }) => {
           className="navbar-toggler"
           data-bs-toggle="collapse"
           data-bs-target="#navmenu"
-          
+
         >
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navmenu">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <a href="#" className="nav-link text-light"                onClick={() => setPage('Top')}
+              <a href="#" className="nav-link text-light" onClick={() => setPage('Top')}
               >Tops</a>
             </li>
             <li className="nav-item">
-              <a href="#" className="nav-link text-light"                onClick={() => setPage('Bottom')}
+              <a href="#" className="nav-link text-light" onClick={() => setPage('Bottom')}
               >Bottoms</a>
             </li>
             <li className="nav-item">
-              <a href="#" className="nav-link text-light "                onClick={() => setPage('Dress')}
+              <a href="#" className="nav-link text-light " onClick={() => setPage('Dress')}
               >Dresses</a>
             </li>
             <li className="nav-item">
-              <a href="#" className="nav-link text-light"                 onClick={() => setPage('Jacket')}
-              >Jackets/Hoodies</a>
+              <a href="#" className="nav-link text-light" onClick={() => setPage('Jacket')}>
+                Jackets/Hoodies
+              </a>
             </li>
             <li className="nav-item">
-              <button
-                className="btn btn-light text-danger"
-                onClick={() => setPage('cart')}
-              >
-                Cart
-              </button>
+              <a href="#" className="nav-link text-light" onClick={() => setPage('cart')}>
+                <i className="fa-solid fa-cart-shopping fa-2x text-white"></i>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#" className="nav-link text-light" onClick={() => setPage('Login')}>
+                <i className="fa-solid fa-user fa-2x text-white"></i>
+              </a>
             </li>
           </ul>
         </div>
