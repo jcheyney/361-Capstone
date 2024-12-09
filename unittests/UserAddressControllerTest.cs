dc5b9ce743c0f0452c451588bcbb559906f635c0
JunKim5959 <xnehdxn@gmail.com>
2024-12-09 12:13:03 -0600

    Update UserAddressControllerTest.cs

--- unittests/UserAddressControllerTest.cs
+++ unittests/UserAddressControllerTest.cs
@@ -1,101 +1,65 @@
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
 using DatabaseApp.Controllers;
 using DatabaseApp.Models.DBClasses;
+using System.Collections.Generic;
 
 namespace DatabaseApp.Tests
 {
-    public class UserAddressControllerTests
+    public class UserAddressControllerTest
     {
-        private readonly UserAddressController _controller;
-
-        public UserAddressControllerTests()
+        private UserAddressController GetController()
         {
-            _controller = new UserAddressController();
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: "UserAddressTestDb")
+                .Options;
+
+            var context = new AppDbContext(options);
+            SeedUserAddresses(context);
+            return new UserAddressController(context);
+        }
+
+        private void SeedUserAddresses(AppDbContext context)
+        {
+            context.UserAddresses.AddRange(new List<UserAddress>
+            {
+                new UserAddress { UserAddressID = 1, Country = "USA", UserState = "CA", Street = "123 Elm St", City = "Los Angeles", ZipCode = "90001" },
+                new UserAddress { UserAddressID = 2, Country = "Canada", UserState = "ON", Street = "456 Maple Ave", City = "Toronto", ZipCode = "M4B1B4" }
+            });
+            context.SaveChanges();
         }
 
         [Fact]
-        public void GetUserAddresses_ShouldReturnAllUserAddresses()
+        public void GetUserAddresses_ShouldReturnAllAddresses()
         {
-            // Act
-            var result = _controller.GetUserAddresses() as OkObjectResult;
+            var controller = GetController();
 
-            // Assert
+            var result = controller.GetUserAddresses().Value.ToList();
+
             Assert.NotNull(result);
-            Assert.Equal(200, result.StatusCode);
-            var addresses = Assert.IsAssignableFrom<IEnumerable<UserAddress>>(result.Value);
-            Assert.NotEmpty(addresses);
+            Assert.Equal(2, result.Count);
         }
 
         [Fact]
-        public void GetUserAddress_ValidId_ShouldReturnUserAddress()
+        public void GetUserAddress_ValidId_ShouldReturnAddress()
         {
-            // Act
-            var result = _controller.GetUserAddress(1) as OkObjectResult;
+            var controller = GetController();
 
-            // Assert
+            var result = controller.GetUserAddress(1).Value;
+
             Assert.NotNull(result);
-            Assert.Equal(200, result.StatusCode);
-            var address = Assert.IsType<UserAddress>(result.Value);
-            Assert.Equal(1, address.userAddressID);
+            Assert.Equal("USA", result.Country);
         }
 
         [Fact]
         public void GetUserAddress_InvalidId_ShouldReturnNotFound()
         {
-            // Act
-            var result = _controller.GetUserAddress(999);
+            var controller = GetController();
 
-            // Assert
+            var result = controller.GetUserAddress(999);
+
             Assert.IsType<NotFoundResult>(result.Result);
-        }
-
-        [Fact]
-        public void CreateUserAddress_ValidUserAddress_ShouldReturnCreatedAddress()
-        {
-            // Arrange
-            var newUserAddress = new UserAddress
-            {
-                country = "Canada",
-                userState = "ON",
-                street = "456 Maple Ave",
-                city = "Toronto",
-                zipcode = "M4B1B4"
-            };
-
-            // Act
-            var result = _controller.CreateUserAddress(newUserAddress) as CreatedAtActionResult;
-
-            // Assert
-            Assert.NotNull(result);
-            Assert.Equal(201, result.StatusCode);
-            var createdAddress = Assert.IsType<UserAddress>(result.Value);
-            Assert.Equal(2, createdAddress.userAddressID); // Assuming this is the second address
-        }
-
-        [Fact]
-        public void CreateUserAddress_NullUserAddress_ShouldReturnBadRequest()
-        {
-            // Act
-            var result = _controller.CreateUserAddress(null);
-
-            // Assert
-            Assert.IsType<BadRequestObjectResult>(result.Result);
-        }
-
-        [Fact]
-        public void CreateUserAddress_InvalidModel_ShouldReturnBadRequest()
-        {
-            // Arrange
-            var invalidUserAddress = new UserAddress();
-            _controller.ModelState.AddModelError("street", "Required");
-
-            // Act
-            var result = _controller.CreateUserAddress(invalidUserAddress);
-
-            // Assert
-            Assert.IsType<BadRequestObjectResult>(result.Result);
         }
     }
 }
