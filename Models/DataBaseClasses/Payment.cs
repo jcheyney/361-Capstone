c5bc96f26b0a2184e90f703e25688424e563f551
JunKim5959 <xnehdxn@gmail.com>
2024-12-09 12:05:07 -0600

    Update Payment.cs

--- Models/DataBaseClasses/Payment.cs
+++ Models/DataBaseClasses/Payment.cs
@@ -1,21 +1,29 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace DatabaseApp.Models.DBClasses
+{
+    public partial class Payment
+    {
+        [Key]
+        public int PaymentID { get; set; }
 
-puiblic partial class Payment{
-    [Required]
-    public int paymentID {get; set;}
+        [Required]
+        [CreditCard]
+        [StringLength(16, MinimumLength = 13, ErrorMessage = "Card number must be between 13 and 16 digits.")]
+        public string CardNumber { get; set; }
 
-    public string cardNumber {get; set;}
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime ExpirationDate { get; set; }
 
-    public localDate expirationDate {get; set;}
+        [Required]
+        [RegularExpression(@"^\d{3,4}$", ErrorMessage = "CVV must be 3 or 4 digits.")]
+        public string CVV { get; set; }
 
-    public string cvv {get; set;}
+        [Required]
+        public int AddressID { get; set; }
 
-    public int addressID {get; set;}
-
-    public int customerID {get; set;}
-
+        [Required]
+        public int CustomerID { get; set; }
+    }
 }
